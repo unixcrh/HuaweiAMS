54039ac1e8bd931ca19cfa85006a29ddd37994ba
Zheng Shen <shenzheng72@hotmail.com>
2015-11-29 23:07:54 +0800

    创建工程

---
+++ Framework/MCS.Library.Cloud.AMSHelper/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCS.Library.Cloud.AMSHelper
+{
+    public class Class1
+    {
+    }
+}
