f46b333e43f61a4695f5503fb219d71fdf5ddba8
Zheng Shen <shenzheng72@hotmail.com>
2015-12-06 13:54:55 +0800

    增加Locks表

---
+++ Database/AMSDB/AMS/Tables/Locks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [AMS].[Locks]
+(
+	[LockID] NVARCHAR(36) NOT NULL PRIMARY KEY, 
+    [ResourceID] NVARCHAR(36) NULL, 
+    [LockPersonID] NVARCHAR(36) NULL, 
+    [LockPersonName] NVARCHAR(255) NULL, 
+    [LockTime] DATETIME NULL DEFAULT GETUTCDATE(), 
+    [EffectiveTime] INT NULL, 
+    [LockType] NVARCHAR(32) NULL, 
+    [Description] NVARCHAR(255) NULL
+)
+
+GO
---
+++ Database/AMSDB/AMS/Tables/Locks_Properties.sql
@@ -0,0 +1,71 @@
+﻿EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'锁ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'LockID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'相关的资源ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'ResourceID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'上锁人的ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'LockPersonID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'上锁人的名称',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'LockPersonName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'上锁的时间',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'LockTime'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'锁的有效期，以秒为单位',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'EffectiveTime'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'锁的类型',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'LockType'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'描述信息',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'Locks',
+    @level2type = N'COLUMN',
+    @level2name = N'Description'
