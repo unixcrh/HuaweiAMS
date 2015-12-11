86a3bce7c41a89e9d3eab50449518ad6fd811e8b
Zheng Shen <shenzheng72@hotmail.com>
2015-12-12 00:51:38 +0800

    调整Azure工程

---
+++ Database/AMSDB/AMS/Tables/UserViews_Properties.sql
@@ -0,0 +1,44 @@
+﻿EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户观看事件记录',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'事件ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'EventID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'UserID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户名称',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'UserName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'创建时间',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'CreateTime'
