bb215bc4a45675c4e45e5a845310d7388727ad04
Zheng Shen <shenzheng72@hotmail.com>
2015-12-11 17:22:19 +0800

    增加用户观看表

---
+++ Database/AMSDB/AMS/Tables/UserViews.sql
@@ -0,0 +1,60 @@
+﻿CREATE TABLE [AMS].[UserViews]
+(
+	[EventID] NVARCHAR(36) NOT NULL , 
+    [UserID] NVARCHAR(36) NOT NULL, 
+	[UserName] NVARCHAR(64) NOT NULL, 
+    [CreateTime] DATETIME NULL DEFAULT GETUTCDATE(), 
+    [LastClientAccessIP] NVARCHAR(255) NULL, 
+    [LastAccessTime] DATETIME NULL DEFAULT GETUTCDATE(), 
+    PRIMARY KEY ([EventID], [UserID])
+)
+
+GO
+
+CREATE INDEX [IX_UserViews_UserID] ON [AMS].[UserViews] ([UserID])
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户观看事件记录',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'事件ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'EventID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'UserID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'用户名称',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'UserName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'创建时间',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'UserViews',
+    @level2type = N'COLUMN',
+    @level2name = N'CreateTime'
