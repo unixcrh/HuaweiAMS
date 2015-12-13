10527aa0bf2f2d9d2a0779d19d3f0a6ab561601f
Zheng Shen <shenzheng72@hotmail.com>
2015-12-14 00:05:28 +0800

    增加测试

---
+++ Framework/MCS.Library.Cloud.AMSHelper/Configuration/StorageSettings.cs
@@ -0,0 +1,48 @@
+﻿using MCS.Library.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCS.Library.Cloud.AMSHelper.Configuration
+{
+    public class StorageSettings : DeluxeConfigurationSection
+    {
+        public static StorageSettings GetConfig()
+        {
+            StorageSettings settings = (StorageSettings)ConfigurationBroker.GetSection("storageSettings");
+
+            if (settings == null)
+                settings = new StorageSettings();
+
+            return settings;
+        }
+
+        [ConfigurationProperty("connectionStrings")]
+        public StorageConnectionStringConfigurationElementCollection ConnectionStrings
+        {
+            get
+            {
+                return (StorageConnectionStringConfigurationElementCollection)this["connectionStrings"];
+            }
+        }
+    }
+
+    public class StorageConnectionStringConfigurationElement : NamedConfigurationElement
+    {
+        [ConfigurationProperty("connectionString", IsRequired = true)]
+        public string ConnectionString
+        {
+            get
+            {
+                return (string)this["connectionString"];
+            }
+        }
+    }
+
+    public class StorageConnectionStringConfigurationElementCollection : NamedConfigurationElementCollection<StorageConnectionStringConfigurationElement>
+    {
+    }
+}
---
+++ Framework/TestProjects/MCS.Library.Cloud.AMSHelper.Test/AMS/StorageTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using MCS.Library.Cloud.AMSHelper.Configuration;
+using MCS.Library.Core;
+using System.Text;
+
+namespace MCS.Library.Cloud.AMSHelper.Test.AMS
+{
+    [TestClass]
+    public class StorageTest
+    {
+        [TestMethod]
+        public void WriteBinaryData()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageSettings.GetConfig().ConnectionStrings.CheckAndGet("amsImages").ConnectionString);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("amsimages");
+            container.CreateIfNotExists(BlobContainerPublicAccessType.Blob); 
+
+            string blobID = UuidHelper.NewUuidString();
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobID);
+
+            string originalData = "Hello world!";
+            byte[] data = Encoding.UTF8.GetBytes(originalData);
+            blockBlob.UploadFromByteArray(data, 0, data.Length);
+
+            string dataLoaded = blockBlob.DownloadText(Encoding.UTF8);
+
+            Assert.AreEqual(originalData, dataLoaded);
+        }
+    }
+}
