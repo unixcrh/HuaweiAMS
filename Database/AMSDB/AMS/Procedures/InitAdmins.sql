5161074269d8b4d722c6d3fdfb0ac167e54fda28
Zheng Shen <shenzheng72@hotmail.com>
2015-12-28 10:35:49 +0800

    增加管理员

--- Database/AMSDB/AMS/Procedures/InitAdmins.sql
+++ Database/AMSDB/AMS/Procedures/InitAdmins.sql
@@ -13,5 +13,14 @@ BEGIN
 
 		INSERT INTO AMS.Admins(UserID, LogonName, [Password], Name)
 		VALUES('D675B6AF-4FF2-43F0-98CD-3629A045333A', 'ronchen@microsoft.com', 'B0-81-DB-E8-5E-1E-C3-FF-C3-D4-E7-D0-22-74-00-CD', '陈荣华');
+
+		INSERT INTO AMS.Admins(UserID, LogonName, [Password], Name)
+		VALUES('D8A5B95B-10AF-4E6F-AAF7-9C14ACEF4B84', 'susan.chensumin@huawei.com', 'B0-81-DB-E8-5E-1E-C3-FF-C3-D4-E7-D0-22-74-00-CD', 'Chen Sumin');
+
+		INSERT INTO AMS.Admins(UserID, LogonName, [Password], Name)
+		VALUES('630F456B-2932-4669-9553-B59E855DD42E', 'david.yangshaojun@huawei.com', 'B0-81-DB-E8-5E-1E-C3-FF-C3-D4-E7-D0-22-74-00-CD', 'Yang Shaojun');
+
+		INSERT INTO AMS.Admins(UserID, LogonName, [Password], Name)
+		VALUES('C5C7BF17-22F9-46E6-8485-35C03FAD429B', 'liuxianping@huawei.com', 'B0-81-DB-E8-5E-1E-C3-FF-C3-D4-E7-D0-22-74-00-CD', 'Li Xianping');
 	END
 END
