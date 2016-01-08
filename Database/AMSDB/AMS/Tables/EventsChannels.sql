f534ba9e75bf8f553101f300a292ae5e2ffa7f9b
Zheng Shen <shenzheng72@hotmail.com>
2016-01-08 21:53:01 +0800

    增加EventsChannels表

---
+++ Database/AMSDB/AMS/Tables/EventsChannels.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [AMS].[EventsChannels]
+(
+	[EventID] NVARCHAR(36) NOT NULL , 
+    [ChannelID] NVARCHAR(36) NOT NULL, 
+    [AMSProgramID] NVARCHAR(50) NULL,
+	[DefaultPlaybackUrl] NVARCHAR(MAX) NULL, 
+    [CDNPlaybackUrl] NVARCHAR(MAX) NULL, 
+    [IsDefault] INT NULL DEFAULT 0, 
+    PRIMARY KEY ([EventID], [ChannelID])
+)
+
+GO
+
+
+
+
+CREATE INDEX [IX_EventsChannels_ChannelID] ON [AMS].[EventsChannels] ([ChannelID])
---
+++ Database/AMSDB/AMS/Tables/EventsChannels_Properties.sql
@@ -0,0 +1,71 @@
+﻿EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'事件和频道对应表',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'事件ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'EventID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'频道ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'ChannelID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'事件的状态',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'State'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'AMS中Program的ID',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'AMSProgramID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'无CDN的视频回放地址',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'DefaultPlaybackUrl'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'带CDN的视频回放地址',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'CDNPlaybackUrl'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'是否是默认频道',
+    @level0type = N'SCHEMA',
+    @level0name = N'AMS',
+    @level1type = N'TABLE',
+    @level1name = N'EventsChannels',
+    @level2type = N'COLUMN',
+    @level2name = N'IsDefault'
