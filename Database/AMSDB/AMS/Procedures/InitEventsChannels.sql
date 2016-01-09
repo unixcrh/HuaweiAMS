1fb21746c41e178eaeb7b684a6b0e8a16e20f693
Zheng Shen <shenzheng72@hotmail.com>
2016-01-09 21:19:31 +0800

    增加[InitEventsChannels]

---
+++ Database/AMSDB/AMS/Procedures/InitEventsChannels.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [AMS].[InitEventsChannels]	
+AS
+BEGIN
+	INSERT INTO AMS.EventsChannels(EventID, ChannelID, [State], IsDefault)
+	SELECT [ID], ChannelID, [State], 1
+	FROM AMS.Events
+END
