30a50d77789058ed02d39bdd235ba60655f2f771
Zheng Shen <shenzheng72@hotmail.com>
2016-01-10 14:36:10 +0800

    增加EventsChannelsView视图

--- Database/AMSDB/AMS/Procedures/InitEventsChannels.sql
+++ Database/AMSDB/AMS/Procedures/InitEventsChannels.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [AMS].[InitEventsChannels]	
 AS
 BEGIN
-	INSERT INTO AMS.EventsChannels(EventID, ChannelID, [State], IsDefault)
-	SELECT [ID], ChannelID, [State], 1
+	INSERT INTO AMS.EventsChannels(EventID, ChannelID, [State], IsDefault, DefaultPlaybackUrl, CDNPlaybackUrl)
+	SELECT [ID], ChannelID, [State], 1, DefaultPlaybackUrl, CDNPlaybackUrl
 	FROM AMS.Events
 END
