60d039b6b4637888ef85f0bc7195d526c4402622
Zheng Shen <shenzheng72@hotmail.com>
2016-01-31 09:33:51 +0800

    增加单元测试

---
+++ Framework/TestProjects/MCS.Library.Cloud.W3.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+
+namespace MCS.Library.Cloud.W3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SamlRequestTest()
+        {
+            XmlDocument xmlDoc = SamlHelper.GetSignedRequestDoc("www.Kenexa.com", string.Empty);
+
+            Console.WriteLine(xmlDoc.OuterXml);
+        }
+    }
+}
